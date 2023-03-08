554b0e25895192c35e1b3975bf02ca93944cbe5e
jisu-yang <jisuyang@egoist.im>
2023-03-09 00:16:21 +0900

    [feat] 링크 연결시 외부 브라우저 설정 추가

--- main.js
+++ main.js
@@ -14,8 +14,28 @@ const createWindow = () => {
         y: 620,
         webPreferences: { preload: path.join(__dirname, 'preload.js') }
     });
-    
-    win.removeMenu()
+
+    // 외부 브라우저로 연결 1
+    // const shell = require('electron').shell;
+
+    // win.webContents.on('will-navigate', (event, url) => {
+    //     event.preventDefault()
+    //     shell.openExternal(url)
+    // });
+
+    // 외부 브라우저로 연결 2 > 강력한 새로고침 먹힘
+    var handleRedirect = (e, url) => {
+        if(url != win.webContents.getURL()) {
+        e.preventDefault()
+        require('electron').shell.openExternal(url)
+        }
+    }
+
+    win.webContents.on('will-navigate', handleRedirect);
+    win.webContents.on('new-window', handleRedirect);
+
+
+    win.removeMenu();
     win.loadFile('index.html');
 };
  
@@ -30,3 +50,4 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
+
